7fe08d6f0cc0a6abf6a17b4bde7c865116eaedee
Andrey Semenov <andrey.semenoff@gmail.com>
2016-05-29 02:27:42 +0300

    Improved gulpfile.js and change .scss, .jade templates

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ var
   gulpif = require('gulp-if'),
   useref = require('gulp-useref'),
   spritesmith = require('gulp.spritesmith'),
+  stripCSS = require('gulp-strip-css-comments'),
   sftp = require('gulp-sftp');
 
 /* --------- paths --------- */
@@ -25,29 +26,36 @@ const
     jade: {
       watch: '_dev/jade/**/*.jade',
       src:   '_dev/jade/_pages/*.jade',
-      dest:  '_build' 
+      dest:  '_build/' 
     },
 
     scss: {
       watch: '_dev/scss/**/*.scss',
       src:   '_dev/scss/*.scss',
-      dest:  '_build/css'
+      dest:  '_build/css/'
+    },
+
+    img: {
+      all: '_dev/img/*',
+      iconsMisc: '_dev/img/icons/*.*',
+      iconsSocial: '_dev/img/icons/social/*.*',
+      dest: '_build/img/'
     },
 
     js: {
       watch: '_dev/js/**/*.js',
       src:   '_dev/js/*.js',
-      dest:  '_build/js'
+      dest:  '_build/js/'
     },
 
     bower: {
       watch:   '_dev/bower/**',
-      cssDest: '_build/css',
-      jsDest:  '_build/js'
+      cssDest: '_build/css/',
+      jsDest:  '_build/js/'
     },
 
     browserSync : {
-      baseDir : '_build',
+      baseDir : '_build/',
       watch : ['_build/*.html', '_build/css/*.css', '_build/js/*.js']
     }
   };
@@ -85,27 +93,52 @@ gulp.task('scss', function() {
     .pipe(scss({
       outputStyle: 'expanded'
     }))
+    .pipe(stripCSS())
     .pipe(gulp.dest(paths.scss.dest))
 })
 
 /* ---------- sprite generator ---------- */
 
-gulp.task('spriteMisc', function() {
+gulp.task('spritesMisc', function() {
     var spriteData = 
-        gulp.src('_dev/img/icons/*.*') // путь, откуда берем картинки для спрайта
+        gulp.src(paths.img.iconsMisc) // путь, откуда берем картинки для спрайта
             .pipe(spritesmith({
                 imgName: '../img/spriteMisc.png',
                 cssName: '_spriteMisc.scss',
-                cssFormat: 'scss'
-                // // algorithm: 'binary-tree',
-                // cssVarMap: function(sprite) {
-                //     sprite.name = 'icon_' + sprite.name
-                // }
+                cssFormat: 'scss',
+                cssSpritesheetName: 'misc',
+                cssVarMap: function (sprite) {
+                  sprite.name = 'icon-' + sprite.name;
+                }
             }));
-
-    spriteData.img.pipe(gulp.dest('_build/img')); // путь, куда сохраняем картинку
+    spriteData.img.pipe(gulp.dest(paths.img.dest)); // путь, куда сохраняем картинку
     spriteData.css.pipe(gulp.dest('_dev/scss/_misc/')); // путь, куда сохраняем стили
 });
+
+gulp.task('spritesSocial', function() {
+    var spriteData = 
+        gulp.src(paths.img.iconsSocial) // путь, откуда берем картинки для спрайта
+            .pipe(spritesmith({
+                imgName: '../img/spriteSocial.png',
+                cssName: '_spriteSocial.scss',
+                cssFormat: 'scss',
+                cssSpritesheetName: 'social',
+                cssVarMap: function (sprite) {
+                  sprite.name = 'icon-' + sprite.name;
+                }
+            }));
+    spriteData.img.pipe(gulp.dest(paths.img.dest)); // путь, куда сохраняем картинку
+    spriteData.css.pipe(gulp.dest('_dev/scss/_misc/')); // путь, куда сохраняем стили
+});
+
+/* ---------- image min ---------- */
+
+gulp.task('imagemin', function() {
+  del(paths.img.dest + '*.*')
+  return gulp.src(paths.img.all)
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.img.dest))
+})
 
 /* --------- js --------- */
 
@@ -138,11 +171,14 @@ gulp.task('sftp', function () {
 gulp.task('watch', function(){
   gulp.watch([paths.jade.watch, paths.bower.watch], ['jade']);
   gulp.watch(paths.scss.watch, ['scss']);
+  gulp.watch(paths.img.all, ['imagemin']);
+  gulp.watch(paths.img.iconsMisc, ['spritesMisc']);
+  gulp.watch(paths.img.iconsSocial, ['spritesSocial']);
   gulp.watch(paths.js.watch, ['js']);
   gulp.watch(paths.browserSync.watch).on('change', browserSync.reload);
 });
 
 /* --------- default --------- */
 
-gulp.task('default', ['jade', 'scss', 'js', 'sync', 'watch']);
+gulp.task('default', ['jade', 'scss', 'imagemin', 'spritesMisc', 'spritesSocial', 'js', 'sync', 'watch']);
 
